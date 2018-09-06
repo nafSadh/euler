fc1e338dc8ea8884a056da6d3cf7be196bbe3191
Khan(Sadh) <kmostafa@tcicredit.com>
2018-09-06 16:14:12 -0400

    003 Quicker

--- java/src/main/java/com/nafsadh/euler/LargestPrimeFactor.java
+++ java/src/main/java/com/nafsadh/euler/LargestPrimeFactor.java
@@ -1,28 +1,47 @@
 package com.nafsadh.euler;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LargestPrimeFactor {
     
-    private List<Long> primes = new ArrayList<>();
+    private List<Long>         primes = new ArrayList<>();
+    private Map<Long, Boolean> isP    = new HashMap<>();
     
     public LargestPrimeFactor() {
         primes.add(2L);
         primes.add(3L);
+        isP.put(1L,false );
+        isP.put(2L,true);
+        isP.put(3L,true );
     }
     
     private boolean isPrime(long x) {
         long cap = (long) Math.sqrt(x);
-        for (int i = 0; i < primes.size() && primes.get(i) < cap; i++) {
+        for (int i = 0; i < primes.size() && primes.get(i) <= cap; i++) {
             if (x % primes.get(i) == 0) {
                 return false;
             }
         }
         return true;
+    }
+    
+    public boolean isPrime2(long x){
+        if(isP.containsKey(x)) return isP.get(x);
+        if(x%2==0) return false;
+        long cap = (long) Math.sqrt(x);
+        for (int i = 3; i <= cap; i+=2) {
+            if(x%i==0){
+                isP.put(x,false );
+                return false;
+            }
+        }
+        isP.put(x,true );
+        return true;
+        
     }
     
     long getIthPrime(int i) {
@@ -47,7 +66,7 @@ public class LargestPrimeFactor {
         while (n>=prime && prime>0){
             if(n%prime==0){
                 maxPF = prime;
-                n=n/prime;
+                while (n%prime==0)n/=prime;
             } else {
                 prime = getIthPrime(++i);
             }
@@ -56,13 +75,30 @@ public class LargestPrimeFactor {
         return maxPF;
     }
     
+    public long largestPrimeFactorFast(long n){
+        long maxPrime = 2;
+        for(int i = 1; i <= (int) Math.sqrt(n); i++) {
+            long mod = n%i;
+            if(mod == 0) {
+                long div = n/i;
+                if(div == i) {
+                    if(isPrime2(div)) maxPrime = Math.max(maxPrime, div);
+                } else {
+                    if(isPrime2(div)) maxPrime = Math.max(maxPrime, div);
+                    else if(isPrime2(i)) maxPrime = Math.max(maxPrime, i);
+                }
+            }
+        }
+        return maxPrime;
+    }
+    
     public static void main(String[] args) {
         LargestPrimeFactor largestPrimeFactor = new LargestPrimeFactor();
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for (int a0 = 0; a0 < t; a0++) {
             long n = in.nextLong();
-            System.out.println(largestPrimeFactor.largestPrimeFactor(n));
+            System.out.println(largestPrimeFactor.largestPrimeFactorFast(n));
         }
     }
 }
